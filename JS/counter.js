67b08cec3c7a827b3e1f3073a4803d3928b7b6f3
ace-09 <nimlohumi34@gmail.com>
2023-06-03 10:08:08 +0530

    Day 4

---
+++ JS/counter.js
@@ -0,0 +1,11 @@
+let counter = 0;
+function hello() {
+  counter++;
+  document.querySelector("h1").innerHTML = counter;
+  if (counter % 10 === 0) {
+    alert(`The counter is ${counter}`);
+  }
+}
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelector("button").onclick = hello;
+});
